1f41cc0f891a9abdc83efba3acdfa3f7edeed9b2
Manas Kumar Pradhan <manas.pradhan@pixelcompute.com>
2026-01-26 23:54:00 +0530

    Used fixtures to make DRY

--- playwright-tests/e2e/tests/login.spec.ts
+++ playwright-tests/e2e/tests/login.spec.ts
@@ -1,16 +1,17 @@
-import { test, expect } from "@playwright/test"
-import LoginPage from "../poms/login"
+// login.spec.js
+
+import { test } from "../fixtures";
 
 test.describe("Login page", () => {
-  test("should login with correct credentials", async ({ page }) => {
-    const loginPage = new LoginPage(page)
-
-    await page.goto("http://localhost:3000")
-
+  test("should login with the correct credentials", async ({
+    page,
+    loginPage,
+  }) => {
+    await page.goto("http://localhost:3000");
     await loginPage.loginAndVerifyUser({
       email: "oliver@example.com",
       password: "welcome",
-      username: "Oliver Smith"
-    })
-  })
-})
+      username: "Oliver Smith",
+    });
+  });
+});
--- playwright-tests/e2e/tests/register.spec.ts
+++ playwright-tests/e2e/tests/register.spec.ts
@@ -1,24 +1,21 @@
-import { test, expect } from "@playwright/test";
-import { faker } from '@faker-js/faker'
-import LoginPage from "../poms/login";
+// register.spec.js
+
+import { test } from "../fixtures";
+import { faker } from "@faker-js/faker";
 
 test.describe("Register page", () => {
-  test("should register a new user", async ({ page }) => {
+  test("should register a new user", async ({ page, loginPage }) => {
     const username = faker.person.fullName();
     const email = faker.internet.email();
     const password = faker.internet.password();
 
-    const loginPage = new LoginPage(page);
-
-    await page.goto("http://localhost:3000")
-    await page.getByTestId('login-register-link').click();
-
-    await page.getByTestId('signup-name-field').fill(username)
-    await page.getByTestId('signup-email-field').fill(email)
-    await page.getByTestId('signup-password-field').fill(password)
-    await page.getByTestId('signup-password-confirmation-field').fill(password)
-    await page.getByTestId('signup-submit-button').click();
-
-    await loginPage.loginAndVerifyUser({ email, password, username })
-  })
-})
+    await page.goto("http://localhost:3000/");
+    await page.getByTestId("login-register-link").click();
+    await page.getByTestId("signup-name-field").fill(username);
+    await page.getByTestId("signup-email-field").fill(email);
+    await page.getByTestId("signup-password-field").fill(password);
+    await page.getByTestId("signup-password-confirmation-field").fill(password);
+    await page.getByTestId("signup-submit-button").click();
+    await loginPage.loginAndVerifyUser({ email, password, username });
+  });
+});
