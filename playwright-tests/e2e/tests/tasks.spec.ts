fca15e0f203bdc9174f9e2579c8e933eb1415844
Manas Kumar Pradhan <manas.pradhan@pixelcompute.com>
2026-02-04 18:55:51 +0530

    Added spec for deleting the task

--- playwright-tests/e2e/tests/tasks.spec.ts
+++ playwright-tests/e2e/tests/tasks.spec.ts
@@ -18,4 +18,15 @@ test.describe("Tasks page", () => {
     await taskPage.createTaskAndVerify({ taskName })
     await taskPage.markTaskAsCompletedAndVerify({ taskName })
   })
+
+  test("should be able to delete a completed task", async ({ page, taskPage }) => {
+    await taskPage.createTaskAndVerify({ taskName })
+    await taskPage.markTaskAsCompletedAndVerify({ taskName })
+    const completedTask = page.getByTestId("tasks-completed-table").getByRole("row", { name: taskName })
+
+    await completedTask.getByTestId("completed-task-delete-link").click()
+
+    await expect(completedTask).toBeHidden()
+    await expect(page.getByTestId("tasks-pending-table").getByRole("row", { name: taskName })).toBeHidden()
+  })
 });
